6bad57f7ee96c978f12419da4e65d00351a1aa46
rafaelfang <cf797@mail.missouri.edu>
2015-03-22 17:52:14 -0500

    experiment driver

--- experimentDriver.m
+++ experimentDriver.m
@@ -1,5 +1,5 @@
 rng(1);
-testContact('T0659.txt');
-testContact('T0696.txt');
-testContact('T0740.txt');
-testContact('T0753.txt');
+totalNumOfChange=testContactMethod2('T0659.txt');
+% testContact('T0696.txt');
+% testContact('T0740.txt');
+% testContact('T0753.txt');
